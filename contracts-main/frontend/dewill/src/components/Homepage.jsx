f77e1ab3a8da9a48eedc3d81234001098f4aa8fa
Omkar Sanas <omkar.sanas91@gmail.com>
2022-12-03 15:57:45 +0530

    homepage

--- contracts-main/frontend/dewill/src/components/Homepage.jsx
+++ contracts-main/frontend/dewill/src/components/Homepage.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import dc from "../assets/discord-handle.svg";
+import tw from "../assets/twitter-handle.svg";
+import yt from "../assets/yt-handle.svg";
+import wallet from "../assets/wallet.svg";
+import lock from "../assets/lock.svg";
+import funds from "../assets/funds.svg";
 
 const Homepage = () => {
   return (
@@ -10,9 +16,27 @@ const Homepage = () => {
       </div>
       <div className="handles-container">
         <div className="handles">
-          <img src="" alt="" srcset="" />
-          <img src="" alt="" srcset="" />
-          <img src="" alt="" srcset="" />
+          <img src={yt} alt="" srcset="" />
+          <img src={tw} alt="" srcset="" />
+          <img src={dc} alt="" srcset="" />
+        </div>
+        <button>Get Started</button>
+        <h2>How does it work?</h2>
+        <div className="desc-container">
+          <div className="one">
+            <img src={wallet} alt="" />
+            <p>Link all of your wallet to pur platform</p>
+          </div>
+          <div className="two">
+            <img src={lock} alt="" />
+            <p>Create ab Inactivity Time Dependent lock</p>
+          </div>
+          <div className="three">
+            <img src={funds} alt="" />
+            <p>
+              Funds transfer automatically when your wallet is found inactive
+            </p>
+          </div>
         </div>
       </div>
     </div>
