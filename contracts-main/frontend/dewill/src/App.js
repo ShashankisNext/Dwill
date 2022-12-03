18f6bc974022368621a3b7eeb19fca47fa705530
Omkar Sanas <omkar.sanas91@gmail.com>
2022-12-03 17:48:57 +0530

    Login Page Completed UI

--- contracts-main/frontend/dewill/src/App.js
+++ contracts-main/frontend/dewill/src/App.js
@@ -1,8 +1,9 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
 import Header from "./components/Header";
 import Homepage from "./components/Homepage";
+import Login from "./components/Login";
 import "./styles/Home.css";
 
 export default function Home() {
-  return <Homepage />;
+  return <Login />;
 }
