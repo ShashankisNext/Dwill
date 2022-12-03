6e9e3b610f45f2249a4fa91ae9221f5d343bb253
Omkar Sanas <omkar.sanas91@gmail.com>
2022-12-03 15:44:59 +0530

    web3 app

---
+++ contracts-main/frontend/dewill/src/App.js
@@ -0,0 +1,7 @@
+import { ConnectWallet } from "@thirdweb-dev/react";
+import Header from "./components/Header";
+import "./styles/Home.css";
+
+export default function Home() {
+  return <Header />;
+}
---
+++ contracts-main/frontend/dewill/src/components/Homepage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div className="container">
+      <div className="intro">
+        <h3>Introducing</h3>
+        <h1>De-Will Protocol</h1>
+        <h3>Recovery is possible now!</h3>
+      </div>
+      <div className="handles-container">
+        <div className="handles">
+          <img src="" alt="" srcset="" />
+          <img src="" alt="" srcset="" />
+          <img src="" alt="" srcset="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
